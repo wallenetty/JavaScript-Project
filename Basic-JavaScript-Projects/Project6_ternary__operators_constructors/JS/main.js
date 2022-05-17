b6e776d10b0e826366656ee6a5422182e9d41986
Jeannette W <wallenetty@hotmail.com>
2022-05-16 20:57:44 -0500

    Update

--- Basic-JavaScript-Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic-JavaScript-Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,3 +1,4 @@
+// Write the following code from step 128
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -5,9 +6,31 @@ function Ride_Function() {
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
+// Create a basic program that outputs wheether or not a user is old enough to vote
 function Vote_Function() {
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
     Can_vote = (Age < 18) ? "You are too young":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
+
+// New Keyword Assignment, Object Contructor Function start
+function Pet(Type, Breed, Ages, Color) {
+        this.Pet_Type = Type;
+        this.Pet_Breed = Breed;
+        this.Pet_Ages = Ages;
+        this.Pet_Color = Color;
+    }
+
+var Jeannette = new Pet("Cat", "Domestic Shorthair", 13, "Tortoiseshell");
+var Barrie = new Pet("Dog", "Springer Spaniel", 6, "Liver & White");
+var Carley = new Pet("Cat", "Domestic Shorthair", 1, "Black");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML = "Barrie owns a " + Barrie.Pet_Color + "-colored " + Barrie.Pet_Type;
+}
+// New Keyword Assignment, Object Contructor Function end
+
+
+//Assign a variable the value of a reserved word and attempt to display in the browser
+var password = password;
+document.write(password); 
