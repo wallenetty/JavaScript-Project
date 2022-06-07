cbc9e2d7baf66492ccf36adc627562276f0346ef
Jeannette W <wallenetty@hotmail.com>
2022-06-07 17:25:29 -0500

    Update

---
+++ JavaScript/Pop_Contact_Form/JS/Pop_Contact_Form.js
@@ -0,0 +1,8 @@
+// Create a popup form, JavaScript components
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
