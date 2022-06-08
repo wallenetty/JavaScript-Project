cb236b4f26250fa99cb144958dc5945dfdaf991b
Jeannette W <wallenetty@hotmail.com>
2022-06-08 16:35:09 -0500

    Update

---
+++ JavaScript/Sticky Challenge/JS/Sticky.js
@@ -0,0 +1,8 @@
+// Sticky Challenge, Create a sticky contact button on the bottom right of a webpage and make it so that a contact form pops up when the stick contact button is clicked 6/8/22
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
